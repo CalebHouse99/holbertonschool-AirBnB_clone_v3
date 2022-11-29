1b59d3fd4a80883b5aab444bb4270d359a02ac5b
claybowl <claybowlproductions@gmail.com>
2022-11-29 08:43:30 -0800

    states

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -43,21 +43,13 @@ def delete_state(state_id):
 @app_views.route('/states/', methods=['POST'], strict_slashes=False)
 def post_state():
     """creates state object"""
-#    if not request.get_json():
-#        abort(400, descritpion="Not a JSON")
-#
-#    if 'name' not in request.get_json():
-#        abort(400, description="Missing name")
+    if not request.get_json():
+        abort(400, description="Not a JSON")
+
+    if 'name' not in request.get_json():
+        abort(400, description="Missing name")
 
     data = request.get_json()
-    if not data:
-        response = make_response(jsonify({"error": "not a JSON"}), 400)
-    return response
-
-    if "name" not in data:
-        response = make_response(jsonify({"error": "Missing name"}), 400)
-    return response
-
     instance = State(**data)
     instance.save()
     return make_response(jsonify(instance.to_dict()), 201)
