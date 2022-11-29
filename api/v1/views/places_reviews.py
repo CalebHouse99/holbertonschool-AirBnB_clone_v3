a309afb3ada9d94ab1ba6ce57711d0c549293892
claybowl <claybowlproductions@gmail.com>
2022-11-29 11:17:34 -0800

    added city import

--- api/v1/views/places_reviews.py
+++ api/v1/views/places_reviews.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """ objects that handle all default RestFul API actions for Reviews """
 from models.review import Review
+from models.city import City
 from models.place import Place
 from models.user import User
 from models import storage
